0d76b31638cc05e81609e40bee24ecaa5032745f
Cedric Poulet <cpoulet@student.42.fr>
2018-09-19 19:22:09 +0200

    moving clockwise DONE

--- static/js/cube.js
+++ static/js/cube.js
@@ -1,6 +1,41 @@
+//testArchi();
+//testVisu();
 main();
 
 const STEP = 64;
+
+function testVisu() {
+    const canvas = document.querySelector('#cube');
+    const engine = new BABYLON.Engine(canvas, true);
+    let scene = createScene(canvas, engine);
+
+    let Rubiks = createRubiks(scene);
+
+    let f = [0,1,2,3,4,5,6,7,8];
+    for (i in f) {
+        Rubiks[f[i]].setParent(Rubiks[13]);
+    }
+    Rubiks[13].rotate(BABYLON.Axis.Z, Math.PI / 4, BABYLON.Space.WORLD);
+
+    engine.runRenderLoop(function() {
+        scene.render();
+     });
+}
+
+function updateArchi(archi, move) {
+    window['rot' + move](archi);
+}
+
+function testArchi() {
+    let archi = createArchi();
+    show(archi)
+    let stack = ['L', 'L', 'L', 'L'].reverse();
+    let move = stack.pop();
+    while (move) {
+        updateArchi(archi, move);
+        move = stack.pop();
+    }
+}
 
 function main() {
     const canvas = document.querySelector('#cube');
@@ -9,21 +44,19 @@ function main() {
 
     let Rubiks = createRubiks(scene);
     let archi = createArchi();
-    
     show(archi)
-    let stack = ['F', 'U'];
-    stack = stack.reverse();
+    
+    let stack = ['F', 'U', 'R', 'R', 'U', 'L'].reverse();
     let i = 0;
-    let move = stack.pop();
+    let move = nextMove(stack, Rubiks, archi);
     scene.registerBeforeRender(function() {
         if (move) {
-            window[move](Rubiks, archi);
-            i++;
-            if (i == STEP) {
+            if (i == STEP / 2) {
                 i = 0;
-                updateArchi(archi, move);
-                resetParents(Rubiks);
-                move = stack.pop();
+                move = nextMove(stack, Rubiks, archi);
+            } else {
+                window[move](Rubiks, archi);
+                i++;
             }
         }
     });
@@ -31,41 +64,57 @@ function main() {
     engine.runRenderLoop(function() {
         scene.render();
      });
+}
 
+function nextMove(stack, Rubiks, archi) {
+    let move = stack.pop();
+    if (move) {
+        console.log("_____", move, "_____");
+        setParents(Rubiks, move, archi);
+        updateArchi(archi, move);
+        show(archi);
+        return move;
+    }
+    return false;
 }
 
 function updateArchi(archi, move) {
-    window['rot' + move](archi);
+    let tmp = archi.slice(0);
+    window['rot' + move](archi, tmp);
 }
 
-function resetParents(Rubiks) {
-    for (let j=0; j < 27; j++) {
-        Rubiks[j].setParent(null);
+function setParents(Rubiks, move, archi) {
+    let set = new Set(window['get' + move](archi));
+    console.log(set);
+    for (let i = 0; i < 27; i++) {
+        if (set.has(i)) {
+            Rubiks[i].setParent(Rubiks[13]);
+        } else {
+            Rubiks[i].setParent(null);
+        }
     }
 }
 
 function U(Rubiks, archi) {
-    let li = getU(archi);
-    console.log('U : ', li);
-    for (i in li) {
-        Rubiks[li[i]].setParent(Rubiks[13]);
-    }
     Rubiks[13].rotate(BABYLON.Axis.Y, Math.PI / STEP, BABYLON.Space.WORLD);
 }
 
 function F(Rubiks, archi) {
-    let li = getF(archi);
-    console.log('F : ', li);
-    for (i in li) {
-        Rubiks[li[i]].setParent(Rubiks[13]);
-    }
     Rubiks[13].rotate(BABYLON.Axis.Z, Math.PI / STEP, BABYLON.Space.WORLD);
+}
+
+function R(Rubiks, archi) {
+    Rubiks[13].rotate(BABYLON.Axis.X, -Math.PI / STEP, BABYLON.Space.WORLD);
+}
+
+function L(Rubiks, archi) {
+    Rubiks[13].rotate(BABYLON.Axis.X, Math.PI / STEP, BABYLON.Space.WORLD);
 }
 
 function createArchi() {
     let archi = [];
     for (let i = 0; i < 27; i++) {
-        archi.push(String(i));
+        archi.push(i);
     }
     return archi;
 }
@@ -83,8 +132,7 @@ function getL(cube) {return cube.filter(function(v, i) {return i % 3 == 0});}
 //  0  9 18
 //  1 10 19
 //  2 11 20
-function rotU(archi) {
-    let tmp = archi.slice(0);
+function rotU(archi, tmp) {
     archi[0] = tmp[2];
     archi[1] = tmp[11];
     archi[2] = tmp[20];
@@ -93,8 +141,6 @@ function rotU(archi) {
     archi[19] = tmp[9];
     archi[18] = tmp[0];
     archi[9] = tmp[1];
-    console.log("U");
-    show(archi);
 }
 
 // 0 1 2
@@ -104,8 +150,7 @@ function rotU(archi) {
 // 6 3 0
 // 7 4 1
 // 8 5 2
-function rotF(archi) {
-    let tmp = archi.slice(0);
+function rotF(archi, tmp) {
     archi[0] = tmp[6];
     archi[1] = tmp[3];
     archi[2] = tmp[0];
@@ -114,29 +159,42 @@ function rotF(archi) {
     archi[7] = tmp[5];
     archi[6] = tmp[8];
     archi[3] = tmp[7];
-    console.log("F");
-    show(archi);
 }
 
 //  2 11 20
 //  5 14 23
 //  8 17 26
 //
-//  0  9 18
-//  1 10 19
-//  2 11 20
-function rotR(archi) {
-    let tmp = archi.slice(0);
-    archi[2] = tmp[0];
-    archi[11] = tmp[9];
-    archi[20] = tmp[18];
-    archi[23] = tmp[19];
+//  8  5  2
+// 17 14 11
+// 26 23 20
+function rotR(archi, tmp) {
+    archi[2] = tmp[8];
+    archi[11] = tmp[5];
+    archi[20] = tmp[2];
+    archi[23] = tmp[11];
     archi[26] = tmp[20];
-    archi[17] = tmp[11];
-    archi[8] = tmp[2];
-    archi[5] = tmp[1];
-    console.log("R");
-    show(archi);
+    archi[17] = tmp[23];
+    archi[8] = tmp[26];
+    archi[5] = tmp[17];
+}
+
+// 18  9  0
+// 21 12  3
+// 24 15  6
+//
+// 24 21 18
+// 15 12  9
+//  6  3  0
+function rotL(archi, tmp) {
+    archi[18] = tmp[24];
+    archi[9] = tmp[21];
+    archi[0] = tmp[18];
+    archi[3] = tmp[9];
+    archi[6] = tmp[0];
+    archi[15] = tmp[3];
+    archi[24] = tmp[6];
+    archi[21] = tmp[15];
 }
 
 /*
@@ -148,7 +206,7 @@ function rotR(archi) {
 */
 function show(archi) {
     function nb(i) {
-        return archi[i].padStart(2) + ' ';
+        return String(archi[i]).padStart(2) + ' ';
     }
     console.log(' '.repeat(6) + nb(18) + nb(19) + nb(20));
     console.log(' '.repeat(3) + nb(9) + nb(10) + nb(11) + nb(23));
